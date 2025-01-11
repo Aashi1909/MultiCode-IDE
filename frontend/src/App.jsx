6e14449260bbcc1758068a25d3750494b343f128
Aashi <aashiaggarwal28552@gmail.com>
2025-01-12 01:00:41 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./App.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 const App = () => {
   return (
-    <div>
+    <>
+    <BrowserRouter>
+      <RouteHandler />
+  
+    </BrowserRouter>
       
-    </div>
+    </>
+  )
+}
+
+const RouteHandler = () => {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+    <div className="container flex flex-col items-center justify-center min-h-screen ">
+    <form  className='w-[25vw] h-[auto] flex flex-col items-center bg-[#0f0e0e] p-[20px] rounded-lg shadow-xl shadow-black/50'>
+
+        </form>
+        
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default SignUp
