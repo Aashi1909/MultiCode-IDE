920446e38d24dcb7532724becb2e5796c6147e47
Aashi <aashiaggarwal28552@gmail.com>
2025-01-18 00:16:58 +0530

    editor css

--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Editor from '@monaco-editor/react';
+import { VscRunCoverage } from "react-icons/vsc";
+
 
 const EditorPage = () => {
+  const [code, setCode] = useState('');
   return (
     <>
     <Navbar />
     <div className='flex items-center justify-between' style={{height:"calc(100vh - 90px)"}}>
     <div className='left w-[50%] h-full '>
-    <Editor theme='vs-dark' height="100%" width="100%" language="javascript" value="print('hello')" />;
+    <Editor onChange={newCode => setCode(newCode)} theme='vs-dark' height="100%" width="100%" language="javascript" value= {code} />;
+    </div>
+    <div className='right p-[15px] w-[50%] h-full bg-[#27272a] '> 
+      <div className='pb-3 border-b-[1px] border-b-[#1e1e1f] flex items-center justify-between px-[30px]'>
+
+      <p className='text-lg'>Output</p>
+      <button className='btnNormal bg-orange-500 flex items-center !w-[70px] text-white'><VscRunCoverage className=' mr-1 !text-2xl'/>Run</button>
+      </div>
+
     </div>
 
     </div>
