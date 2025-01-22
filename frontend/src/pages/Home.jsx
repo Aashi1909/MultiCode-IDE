ef0c0e501bca61420ebb8f3c34dfdb46571e06c1
Aashi <aashiaggarwal28552@gmail.com>
2025-01-22 23:27:45 +0530

    home.js

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,12 +17,13 @@ const Home = () => {
   const [isCreateModel, setIsCreateModel] = useState(false)
   const [languageOptions, setLanguageOptions] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState(null); 
+  const [projects, setProjects] = useState(null)
+  const [isEditModelShow, setIsEditModelShow] = useState(false);
+
 
   const navigate = useNavigate()
 
   const [name, setName] = useState('')
-
-
 
   const customStyles = {
     control: (provided) => ({
@@ -55,6 +56,26 @@ const Home = () => {
     }),
   };
 
+  const getProjects = async() => {
+    fetch(api_base_url + "/getProjects", {
+      mode:"cors",
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        token: localStorage.getItem("token")
+      })
+    }).then(res => res.json()).then(data => {
+      if(data.success){
+        setProjects(data.projects)
+      }
+      else{
+        toast.error(data.msg)
+      }
+    })
+  }
+
   const getRunTimes = async()=> {
     let res = await fetch("https://emkc.org/api/v2/piston/runtimes");
     let data = await res.json();
@@ -86,6 +107,7 @@ const Home = () => {
   };
 
   useEffect(() =>{
+    getProjects();
     getRunTimes();
   }, [])
 
@@ -103,7 +125,7 @@ const Home = () => {
       })
     }).then(res =>res.json()).then(data =>{
       if(data.success){
-        navigate("/editor/ + data.projectId")
+        navigate("/editor/" + data.projectId)
       }
       else{
         toast.error(data.msg)
@@ -112,6 +134,60 @@ const Home = () => {
     })
 
   }
+  const deleteProject = (id) => {
+    let conf = confirm("Are you sure you want to delete this project?");
+    if (conf) {
+      fetch(api_base_url + "/deleteProject", {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          projectId: id,
+          token: localStorage.getItem("token")
+        })
+      }).then(res => res.json()).then(data => {
+        if (data.success) {
+          getProjects();
+        }
+        else {
+          toast.error(data.msg);
+        }
+      });
+    }
+  };
+
+  const [editProjId, setEditProjId] = useState("");
+
+  const updateProj = () => {
+    fetch(api_base_url + "/editProject", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        projectId: editProjId,
+        token: localStorage.getItem("token"),
+        name: name,
+      })
+    }).then(res => res.json()).then(data => {
+      if (data.success) {
+        setIsEditModelShow(false);
+        setName("");
+        setEditProjId("");
+        getProjects();
+      }
+      else {
+        toast.error(data.msg);
+        setIsEditModelShow(false);
+        setName("");
+        setEditProjId("");
+        getProjects();
+      }
+    })
+  };
 
   return (
     <>
@@ -124,36 +200,58 @@ const Home = () => {
     </div>
     </div>
 
-    <div className='projects px-[100px] mt-5'>
-      <div className='project w-full p-[15px] flex items-center justify-between bg-[#0f0e0e]' >
-        <div className='flex items-center gap-[15px]'>
-        <img src={python}  className='w-[100px] h-[100px] object-cover' alt="" />
-        <div>
-          <h3 className='text-xl !text-[black'>Python Project</h3>
-          <p className='text-[15px] !text-[gray]'>5 Jan, 2025</p>
-        </div>
-        </div>
+    <div className="projects px-[100px] mt-5 pb-10">
 
-        <div className='flex items-center gap-[15px]'>
-          <button className='btnNormal bg-[#0f1026]  hover:bg-orange-600 font-semibold flex items-center !w-[100%] text-lg'><MdEdit className='mr-2'/> Edit</button>
-          <button className='btnNormal bg-[#0f1026]  hover:bg-orange-600 font-semibold !w-[100%] text-lg flex items-center'><MdDeleteOutline className='mr-2 !text-xl'/>Delete</button>
-        </div>
-        
+        {
+          projects && projects.length > 0 ? projects.map((project, index) => {
+            return <>
+              <div className="project w-full p-[15px] flex items-center justify-between bg-[#0f0e0e]">
+                <div onClick={() => { navigate("/editior/" + project._id) }} className='flex w-full items-center gap-[15px]'>
+                  {
+                    project.projLanguage === "python" ?
+                      <>
+                        <img className='w-[130px] h-[100px] object-cover' src="https://images.ctfassets.net/em6l9zw4tzag/oVfiswjNH7DuCb7qGEBPK/b391db3a1d0d3290b96ce7f6aacb32b0/python.png" alt="" />
+                      </>
+                      : project.projLanguage === "javascript" ?
+                        <>
+                          <img className='w-[130px] h-[100px] object-cover' src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="" />
+                        </> : project.projLanguage === "cpp" ?
+                          <>
+                            <img className='w-[130px] h-[100px] object-cover' src="https://upload.wikimedia.org/wikipedia/commons/3/32/C%2B%2B_logo.png" alt="" />
+                          </> : project.projLanguage === "c" ?
+                            <>
+                              <img className='w-[130px] h-[100px] object-cover' src="https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png" alt="" />
+                            </> : project.projLanguage === "java" ?
+                              <>
+                                <img className='w-[130px] h-[100px] object-cover' src="https://static-00.iconduck.com/assets.00/java-icon-1511x2048-6ikx8301.png" alt="" />
+                              </> : project.projLanguage === "bash" ?
+                                <>
+                                  <img className='w-[130px] h-[100px] object-cover' src="https://w7.pngwing.com/pngs/48/567/png-transparent-bash-shell-script-command-line-interface-z-shell-shell-rectangle-logo-commandline-interface-thumbnail.png" alt="" />
+                                </> : ""
+                  }
+                  <div>
+                    <h3 className='text-xl'>{project.name}</h3>
+                    <p className='text-[14px] text-[gray]'>{new Date(project.date).toDateString()}</p>
+                  </div>
+                </div>
 
+                <div className="flex items-center gap-[15px]">
+                  <button className="btnNormal bg-blue-500 transition-all hover:bg-blue-600" onClick={() => {
+                    setIsEditModelShow(true);
+                    setEditProjId(project._id);
+                    setName(project.name);
+                  }}>Edit</button>
+                  <button onClick={() => { deleteProject(project._id) }} className="btnNormal bg-red-500 transition-all hover:bg-red-600">Delete</button>
+                </div>
+              </div>
+            </>
+          }) : "No Project Found !"
+        }
       </div>
-
-
-    </div>
 
     {
       isCreateModel &&
-      <div 
-      // onClick={(e) => {
-      //   if(e.target.classList.contains("modelContainer")){
-      //     setIsCreateModel(false)
-      //   }
-      // }}
-       className=' modelContainer flex flex-col items-center justify-center w-screen h-screen fixed top-0 left-0 bg-[rgba(0,0,0,0.5)]'> 
+      <div className=' modelContainer flex flex-col items-center justify-center w-screen h-screen fixed top-0 left-0 bg-[rgba(0,0,0,0.5)]'> 
       <div className='modelBox flex flex-col items-start w-[50vh] h-[40vh] p-[20px] rounded-xl bg-white'>
       <div className="flex items-center justify-between w-full">
       <h3 className="text-2xl text-black font-semibold">Create Project</h3>
@@ -176,11 +274,29 @@ const Home = () => {
 
       </div>
       </div>  
-
     }
-      
-    </>
-  )
-}
+    
+      {
+        isEditModelShow &&
+        <div onClick={(e) => {
+          if (e.target.classList.contains("modelCon")) {
+            setIsEditModelShow(false);
+            setName("");
+          }
+        }} className='modelCon flex flex-col items-center justify-center w-screen h-screen fixed top-0 left-0 bg-[rgba(0,0,0,0.5)]'>
+          <div className="modelBox flex flex-col items-start rounded-xl p-[20px] w-[25vw] h-[auto] bg-[#0F0E0E]">
+            <h3 className='text-xl font-bold text-center'>Update Project</h3>
+            <div className="inputBox">
+              <input onChange={(e) => { setName(e.target.value) }} value={name} type="text" placeholder='Enter your project name' className="text-black" />
+            </div>
 
-export default Home
+            <button onClick={updateProj} className="btnNormal bg-blue-500 transition-all hover:bg-blue-600 mt-2">Update</button>
+
+          </div>
+        </div>
+      }
+    </>
+  );
+};
+
+export default Home;
