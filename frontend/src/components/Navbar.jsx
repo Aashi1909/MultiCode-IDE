cb2cddef96ec844d6fcbbbf1f86023d28a2abf12
Aashi <aashiaggarwal28552@gmail.com>
2025-01-26 23:09:47 +0530

    some chamges

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,13 +5,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <div className='nav px-[100px] flex items-center justify-between h-[90px] bg-[#0f1026]'>
+    <div className='nav px-[100px] flex items-center justify-between h-[90px] bg-white text-black shadow-2xl'>
       <img className='w-[100px] object-cover' src={logo} alt="" />
       <div className="links flex items-center gap-[15px]">
-          <Link className=' transition-all hover:text-blue-500'>Home</Link>
-          <Link className=' transition-all hover:text-blue-500'>About</Link>
-          <Link className=' transition-all hover:text-blue-500'>Services</Link>
-          <Link className=' transition-all hover:text-blue-500'>Contact</Link>
           <button onClick={()=>{
             localStorage.removeItem("token");
             localStorage.removeItem("isLoggedIn");
