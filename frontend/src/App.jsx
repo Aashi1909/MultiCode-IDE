749c00317133a07ebed9b84e45bf84b8453cde44
Aashi <aashiaggarwal28552@gmail.com>
2025-05-04 17:05:57 +0530

    theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-// App.jsx
-import React, { useEffect, useState } from 'react';
+import  { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import EditorPage from './pages/EditorPage';
-import { ThemeProvider } from './context/ThemeContext';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(
@@ -14,21 +12,19 @@ const App = () => {
   );
 
   const handleLogin = () => {
-    localStorage.setItem("isLoggedIn", "true"); // ensure it's a string
+    localStorage.setItem("isLoggedIn", "true"); 
     setIsLoggedIn(true);
   };
 
 
   return (
     <BrowserRouter>
-      <ThemeProvider>
         <Routes>
           <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/login" element={<Login onLogin={handleLogin} />} />
           <Route path="/editor/:id" element={isLoggedIn ? <EditorPage /> : <Navigate to="/login" />} />
         </Routes>
-      </ThemeProvider>
     </BrowserRouter>
   );
 };
